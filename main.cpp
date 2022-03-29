310d3404122bdc2ff5fedb12a599ae5c11ebb887
EldiShaba <101250259+EldiShaba@users.noreply.github.com>
2022-03-29 21:18:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b, x, y;
-    cin >> a >> b >> x >> y;
+    int a=0,b=0,x=0,y=0;
+    cin>>a>>b>>x>>y;
     if (x<0 ; y>0){
-        cout << a*x-b*y<<endl;
-        if (x>=0; y<=0)
-        cout <<a*(x*x)-b*y << endl;
-    }
+        cout << a*x-b*y<<endl;}
+    if (x>=0; y<=0){
+        cout <<a*(x*x)-b*y << endl;}
     else{
-        cout << a*x+b*(y*y) << endl;
-    }
+        cout << a*x+b*(y*y) << endl;}
     return 0;
     }
